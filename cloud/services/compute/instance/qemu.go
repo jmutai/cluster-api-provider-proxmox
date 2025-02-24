aedf1463f6c1c75e044db2d2f9e0382a4ad5a254
Josphat Mutai <josphatkmutai@gmail.com>
2025-02-24 18:45:46 +0300

    Fix errors

--- cloud/services/compute/instance/qemu.go
+++ cloud/services/compute/instance/qemu.go
@@ -12,8 +12,7 @@ import (
 )
 
 const (
-	bootDevice   = "scsi0"
-	maxScsiDisks = 6 // Proxmox SCSI disk limit
+	bootDvice = "scsi0"
 )
 
 // reconciles QEMU instance
@@ -28,18 +27,19 @@ func (s *Service) reconcileQEMU(ctx context.Context) (*proxmox.VirtualMachine, e
 			return nil, err
 		}
 
-		// No QEMU found, create new one
-		log.V(3).Info("QEMU wasn't found. Creating new QEMU...")
+		// no qemu found, try to create new one
+		log.V(3).Info("qemu wasn't found. new qemu will be created")
 		if exist, err := s.client.VirtualMachineExistsWithName(ctx, s.scope.Name()); exist || err != nil {
 			if exist {
-				err = fmt.Errorf("QEMU %s already exists", s.scope.Name())
+				// there should no qemu with same name. occuring an error
+				err = fmt.Errorf("qemu %s already exists", s.scope.Name())
 			}
-			log.Error(err, "Stopping QEMU creation to avoid duplicate instances")
+			log.Error(err, "stop creating new qemu to avoid replicating same qemu")
 			return nil, err
 		}
 		qemu, err = s.createQEMU(ctx)
 		if err != nil {
-			log.Error(err, "Failed to create QEMU")
+			log.Error(err, "failed to create qemu")
 			return nil, err
 		}
 	}
@@ -52,10 +52,10 @@ func (s *Service) reconcileQEMU(ctx context.Context) (*proxmox.VirtualMachine, e
 	return qemu, nil
 }
 
-// Get QEMU instance from VM ID
+// get QEMU gets proxmox vm from vmid
 func (s *Service) getQEMU(ctx context.Context) (*proxmox.VirtualMachine, error) {
 	log := log.FromContext(ctx)
-	log.Info("Fetching QEMU from VM ID")
+	log.Info("getting qemu from vmid")
 	vmid := s.scope.GetVMID()
 	if vmid != nil {
 		return s.client.VirtualMachine(ctx, *vmid)
@@ -63,34 +63,34 @@ func (s *Service) getQEMU(ctx context.Context) (*proxmox.VirtualMachine, error) 
 	return nil, rest.NotFoundErr
 }
 
-// Create QEMU instance
 func (s *Service) createQEMU(ctx context.Context) (*proxmox.VirtualMachine, error) {
 	log := log.FromContext(ctx)
-	log.Info("Creating QEMU instance")
+	log.Info("creating qemu")
 
-	// Generate QEMU options
+	// create qemu
+	log.Info("making qemu spec")
 	vmoption := s.generateVMOptions()
+	// bind annotation key-values to context
 	schedCtx := framework.ContextWithMap(ctx, s.scope.Annotations())
 	result, err := s.scheduler.CreateQEMU(schedCtx, &vmoption)
 	if err != nil {
-		log.Error(err, "Failed to schedule QEMU instance")
+		log.Error(err, "failed to schedule qemu instance")
 		return nil, err
 	}
-
 	node, vmid, storage := result.Node(), result.VMID(), result.Storage()
 	s.scope.SetNodeName(node)
 	s.scope.SetVMID(vmid)
 
-	// Inject final storage
+	// inject storage
 	s.injectVMOption(&vmoption, storage)
 	s.scope.SetStorage(storage)
 
-	// Set OS image
+	// os image
 	if err := s.setCloudImage(ctx); err != nil {
 		return nil, err
 	}
 
-	// Create QEMU instance
+	// actually create qemu
 	vm, err := s.client.CreateVirtualMachine(ctx, node, vmid, vmoption)
 	if err != nil {
 		return nil, err
@@ -99,7 +99,6 @@ func (s *Service) createQEMU(ctx context.Context) (*proxmox.VirtualMachine, erro
 	return vm, nil
 }
 
-// Generate VM options
 func (s *Service) generateVMOptions() api.VirtualMachineCreateOptions {
 	vmName := s.scope.Name()
 	snippetStorageName := s.scope.GetClusterStorage().Name
@@ -107,35 +106,44 @@ func (s *Service) generateVMOptions() api.VirtualMachineCreateOptions {
 	network := s.scope.GetNetwork()
 	hardware := s.scope.GetHardware()
 	options := s.scope.GetOptions()
-
 	cicustom := fmt.Sprintf("user=%s:%s", snippetStorageName, userSnippetPath(vmName))
 	ide2 := fmt.Sprintf("file=%s:cloudinit,media=cdrom", imageStorageName)
+	// scsi0 := fmt.Sprintf("%s:0,import-from=%s", imageStorageName, rawImageFilePath(s.scope.GetImage()))
 	net0 := hardware.NetworkDevice.String()
-
-	// Assign root SCSI disk
-	scsiDisks := api.Scsi{}
-	scsiDisks.Scsi0 = fmt.Sprintf("%s:0,import-from=%s", imageStorageName, rawImageFilePath(s.scope.GetImage()))
-
-	// Assign additional disks
+	// Assign primary SCSI disk
+	scsiDisks := api.Scsi{
+		Scsi0: fmt.Sprintf("%s:0,import-from=%s", imageStorageName, rawImageFilePath(s.scope.GetImage())),
+	}
+	// Assign additional disks manually
 	extraDisks := s.scope.GetHardware().ExtraDisks
-	if len(extraDisks) > maxScsiDisks {
-		log.FromContext(context.TODO()).Error(fmt.Errorf("too many extra disks"), "Only 6 SCSI disks are supported, ignoring extra disks")
-		extraDisks = extraDisks[:maxScsiDisks] // Limit to 6 disks
+	if len(extraDisks) > 5 {
+		log.FromContext(context.TODO()).Error(fmt.Errorf("too many extra disks"), "Only 6 extra disks are supported, ignoring extra disks")
+		extraDisks = extraDisks[:6] // Trim to 5 disks
 	}
 
 	for i, disk := range extraDisks {
-		slot := i + 1 // scsi1, scsi2, scsi3...
-		scsiDisks.Set(fmt.Sprintf("Scsi%d", slot), fmt.Sprintf("%s:%d,size=%s", disk.Storage, slot, disk.Size))
+		switch i {
+		case 0:
+			scsiDisks.Scsi1 = fmt.Sprintf("%s:%d,%s", disk.Storage, i+1, disk.Size)
+		case 1:
+			scsiDisks.Scsi2 = fmt.Sprintf("%s:%d,%s", disk.Storage, i+1, disk.Size)
+		case 2:
+			scsiDisks.Scsi3 = fmt.Sprintf("%s:%d,%s", disk.Storage, i+1, disk.Size)
+		case 3:
+			scsiDisks.Scsi4 = fmt.Sprintf("%s:%d,%s", disk.Storage, i+1, disk.Size)
+		case 4:
+			scsiDisks.Scsi5 = fmt.Sprintf("%s:%d,%s", disk.Storage, i+1, disk.Size)
+		case 5:
+			scsiDisks.Scsi6 = fmt.Sprintf("%s:%d,%s", disk.Storage, i+1, disk.Size)
+		}
 	}
-
-	// Define VM options
 	vmoptions := api.VirtualMachineCreateOptions{
 		ACPI:          boolToInt8(options.ACPI),
 		Agent:         "enabled=1",
 		Arch:          api.Arch(options.Arch),
 		Balloon:       options.Balloon,
 		BIOS:          string(hardware.BIOS),
-		Boot:          fmt.Sprintf("order=%s", bootDevice),
+		Boot:          fmt.Sprintf("order=%s", bootDvice),
 		CiCustom:      cicustom,
 		Cores:         hardware.CPU,
 		Cpu:           hardware.CPUType,
@@ -176,7 +184,6 @@ func (s *Service) generateVMOptions() api.VirtualMachineCreateOptions {
 	return vmoptions
 }
 
-// Convert bool to int8
 func boolToInt8(b bool) int8 {
 	if b {
 		return 1
@@ -184,20 +191,37 @@ func boolToInt8(b bool) int8 {
 	return 0
 }
 
-// Inject final storage into VM options
 func (s *Service) injectVMOption(vmOption *api.VirtualMachineCreateOptions, storage string) *api.VirtualMachineCreateOptions {
+	// storage is finalized after node scheduling so we need to inject storage name here
 	ide2 := fmt.Sprintf("file=%s:cloudinit,media=cdrom", storage)
 	vmOption.Ide.Ide2 = ide2
 	vmOption.Storage = storage
 
+	// scsi0 := fmt.Sprintf("%s:0,import-from=%s", storage, rawImageFilePath(s.scope.GetImage()))
+	// vmOption.Scsi.Scsi0 = scsi0
 	// Assign primary root disk
 	vmOption.Scsi.Scsi0 = fmt.Sprintf("%s:0,import-from=%s", storage, rawImageFilePath(s.scope.GetImage()))
 
-	// Assign extra disks
+	// Assign Extra Disks
 	extraDisks := s.scope.GetHardware().ExtraDisks
-	for i, disk := range extraDisks {
-		slot := i + 1
-		vmOption.Scsi.Set(fmt.Sprintf("Scsi%d", slot), fmt.Sprintf("%s:%d,size=%s", disk.Storage, slot, disk.Size))
+	if len(extraDisks) > 0 {
+		if len(extraDisks) > 5 {
+			return nil // Returning nil indicates an error
+		}
+		for i, disk := range extraDisks {
+			switch i {
+			case 0:
+				vmOption.Scsi.Scsi1 = fmt.Sprintf("%s:%d,%s", disk.Storage, i+1, disk.Size)
+			case 1:
+				vmOption.Scsi.Scsi2 = fmt.Sprintf("%s:%d,%s", disk.Storage, i+1, disk.Size)
+			case 2:
+				vmOption.Scsi.Scsi3 = fmt.Sprintf("%s:%d,%s", disk.Storage, i+1, disk.Size)
+			case 3:
+				vmOption.Scsi.Scsi4 = fmt.Sprintf("%s:%d,%s", disk.Storage, i+1, disk.Size)
+			case 4:
+				vmOption.Scsi.Scsi5 = fmt.Sprintf("%s:%d,%s", disk.Storage, i+1, disk.Size)
+			}
+		}
 	}
 
 	return vmOption
